a5c260b8640aa1f383ce49b9f1a0ad893b950d85
jhlu <jhlu@kingdonsoft.com>
2025-10-14 15:36:41 +0800

    perf:temp

--- app/core/security.py
+++ app/core/security.py
@@ -4,7 +4,7 @@ from jose import JWTError, jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from app.config import settings
-from app.services.user_service import UserService
+# from app.services.user_service import UserService
 from app.core.database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -20,25 +20,25 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
-async def get_current_user(
-    token: str = Depends(oauth2_scheme),
-    db: AsyncSession = Depends(get_db)
-):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-        email: str = payload.get("sub")
-        if email is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-        
-    user_service = UserService(db)
-    user = await user_service.get_user_by_email(email)
-    if user is None:
-        raise credentials_exception
-    return user 
+# async def get_current_user(
+#     token: str = Depends(oauth2_scheme),
+#     db: AsyncSession = Depends(get_db)
+# ):
+#     credentials_exception = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Could not validate credentials",
+#         headers={"WWW-Authenticate": "Bearer"},
+#     )
+#     try:
+#         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+#         email: str = payload.get("sub")
+#         if email is None:
+#             raise credentials_exception
+#     except JWTError:
+#         raise credentials_exception
+#
+#     # # user_service = UserService(db)
+#     # user = await user_service.get_user_by_email(email)
+#     # if user is None:
+#     #     raise credentials_exception
+#     return user
